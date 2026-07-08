292223734aec681f956cd1796653fe320688ac62
Swarnendu Sekhar Ghosh <swarnendughosh92@gmail.com>
2026-07-08 20:05:04 +0200

    ssg_08072026_2005

--- Diffusion/Model.py
+++ Diffusion/Model.py
@@ -180,43 +180,58 @@ features from the encoder
 """
 
 # This is the self attention block of the diffusion U-Net : the job is to let every pixel/location
-# look at every other pixel/location and decide what information is important.
+# look at every other pixel/location and decide what information is important. Normal convolution mostly looks
+# locally, but attention can look globally.
+# attention = softmax(q x k)
+# output = attention x v
+# finally return x + h , this is a residula connection
 
 class AttnBlock(nn.Module):
     def __init__(self, in_ch): 
         super().__init__()
-        self.group_norm = nn.GroupNorm(32,in_ch)
+        self.group_norm = nn.GroupNorm(32,in_ch) # in_ch must be divisible by 32
         self.proj_q = nn.Conv2d(in_ch, in_ch, 1, stride=1, padding=0)
         self.proj_k = nn.Conv2d(in_ch, in_ch, 1, stride=1, padding=0)
         self.proj_v = nn.Conv2d(in_ch,in_ch,1 , stride=1, padding=0)
         self.proj = nn. Conv2d(in_ch, in_ch, 1, stride=1, padding=0)
+        
         self.initialize()
+    
 
     def initialize(self):
         for module in [self.proj_q, self.proj_k,self.proj_v, self.proj]:
             init.xavier_uniform_(module.weight)
             init.zeros_(module.bias)
+        
         init.xavier_uniform_(self.proj.weight, gain=1e-5)
 
     def forward(self,x):
         B,C,H,W = x.shape
-        h  = self.group_norm(x)
-        q = self.proj_q(h)
+
+        h  = self.group_norm(x)  # normalize all the feature map before attention
+
+        # create q, k , v
+        q = self.proj_q(h)    
         k = self.proj_k(h)
         v = self.proj_v(h)
 
         q = q.permute(0,2,3,1).view(B, H*W, C)
         k = k.view(B,C,H*W)
-        w = torch.bmm(q,k)*(int(C) ** (-0.5))
-        assert list(w.shape) == [B,H * W,H * W]
-        W = F.softmax(w, dim=-1)
+
+        w = torch.bmm(q,k)*(int(C) ** (-0.5)) # batch matrix multiplication (B, H*W,C) X (B,C,H*W) = (B, H*W, H*W)
+                                              # the scaling term c **(-0.5) keeps the attention scores stable
+        assert list(w.shape) == [B,H * W,H * W] # explaination()
+        w = F.softmax(w, dim=-1) # compute attention scores
         
+        
+        v = v.permute(0,2,3,1).view(B,H*W,C) # reshape "value"
 
-        v = v.permute(0,2,3,1).view(B,H*W,C)
-        h = torch.bmm(w,v)
-        assert list(h.shape) == [B,H*W,C]
-        h = h.view(B,H,W,C).permute(0,3,1,2)
-        h = self.proj(h)
+        h = torch.bmm(w,v) # batch matrix multiplication (every location receives information from all other locations)
+        assert list(h.shape) == [B,H*W,C] 
+        h = h.view(B,H,W,C).permute(0,3,1,2) # Here, (B, H*W, C) becomes (B,C,H,W) so it is again normal image-like feature map
+        h = self.proj(h) # another 1x1 convolution. This mixes the attended channel information
 
-        return x + h
+        return x + h   # original feature map + attention-modified feature map
+                       # this doesnot completely replace "x", but it adds useful global information to it.
+                        
 
