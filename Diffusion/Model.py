43074bac3dfbf10cb83d3341e14f22bc3c762ddc
Swarnendu Sekhar Ghosh <swarnendughosh92@gmail.com>
2026-07-14 16:29:08 +0200

    ssg_14072026_1630

--- Diffusion/Model.py
+++ Diffusion/Model.py
@@ -437,3 +437,44 @@ class UNet(nn.Module):
         )             
         self.initialize()
         
+    def initialize(self):
+        init.xavier_uniform_(self.head.weight)
+        init.zeros_(self.head.bias)
+        init.xavier_uniform_(self.tail[-1].weight, gain = 1e-5)
+        init.zeros_(self.tail[-1].bias)
+
+    def forward(self, x, t):
+        # Timestep embedding
+        temb = self.time_embedding(t)
+        # Downsampling
+        h = self.head(x)
+        hs = [h]
+        for layer in self.downblocks:
+            h = layer(h,temb)
+            hs.append(h)
+
+        # Middle
+        for layer in self.middleblocks:
+            h = layer(h,temb)
+        # Upsampling
+        for layer in self.upblocks:
+            if isinstance(layer, ResBlock):
+                h = torch.cat([h, hs.pop()], dim = 1)
+            h = layer(h, temb)
+        h = self.tail(h)
+
+        assert len(hs) ==0
+        return h             
+
+if __name__ == '__main__' :
+        batch_size = 8
+        model = UNet(
+            T = 1000, ch = 128, ch_mult = [1,2,2,2], attn = [1],
+            num_res_blocks = 2, dropout = 0.1)
+        x = torch.randn(batch_size, 3, 32,32)
+        t = torch.randint(1000, (batch_size,))
+        y = model(x,t)
+        print(y.shape)          
+
+
+        
