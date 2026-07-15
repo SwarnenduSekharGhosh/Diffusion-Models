8d2e2657ebdeb1b4e85eabe464f35eccd04015ec
Swarnendu Sekhar Ghosh <swarnendughosh92@gmail.com>
2026-07-15 19:07:34 +0200

    ssg_15072026_1908

--- Diffusion/Train.py
+++ Diffusion/Train.py
@@ -26,42 +26,99 @@ def train(modelConfig : Dict):
     dataloader = DataLoader(dataset, batch_size = modelConfig["batch_size"], shuffle = True, num_workers = 4, drop_last = True, pin_memory = True)
     
     # model setup
-    net_model = UNet(T= modelConfig["T"], ch=modelConfig["channel"], ch_mult=modelConfig["channel_mult"], attn=modelConfig["attn"],
-                     num_res_blocks=modelConfig["num_res_blocks"], dropout=modelConfig["dropout"]).to(device)
+    net_model = UNet(T= modelConfig["T"], 
+                     ch=modelConfig["channel"], 
+                     ch_mult=modelConfig["channel_mult"], 
+                     attn=modelConfig["attn"],
+                     num_res_blocks=modelConfig["num_res_blocks"], 
+                     dropout=modelConfig["dropout"]
+                     ).to(device)
+    
+    # Load existing weights only when provided
     if modelConfig["training_load_weight"] is not None:
-        net_model.load_state_dict(torch.load(os.path.join(
-            modelConfig["save_weight_dir"], modelConfig["training_load_weight"]), map_location=device))
-        optimizer = torch.optim.AdamW(
-            net_model.parameters(), lr = modelConfig["lr"], weight_decay=1e-4)
-        cosineScheduler = optim.lr_scheduler.CosineAnnealingLR(
-            optimizer=optimizer, T_max = modelConfig["epoch"],eta_min=0,last_epoch= -1)
-        warmUpScheduler = GradualWarmupScheduler(
-            optimizer = optimizer, multiplier=modelConfig["multiplier"],warm_epoch= modelConfig["epoch"] //10, after_scheduler=cosineScheduler)
-        trainer = GaussianDiffusionTrainer(
-            net_model, modelConfig["beta_1"],modelConfig["beta_T"],modelConfig["T"]).to(device)
+        checkpoint_path = os.path.join(
+             modelConfig["save_weight_dir"],
+             modelConfig["training_load_weight"]
+        )
+        net_model.load_state_dict(
+             torch.load(checkpoint_path, map_location=device)
+        )
+
+        print(f"Loaded weights from:{checkpoint_path}")
+
+        #net_model.load_state_dict(
+            #torch.load(os.path.join(
+            #modelConfig["save_weight_dir"], 
+            #modelConfig["training_load_weight"]), map_location=device))
         
+    # Optimizer 
+    optimizer = torch.optim.AdamW(
+            net_model.parameters(), 
+            lr = modelConfig["lr"],
+              weight_decay=1e-4)
+    
+    # Learning-rate schedulers
+    cosineScheduler = optim.lr_scheduler.CosineAnnealingLR(
+            optimizer=optimizer, 
+            T_max = modelConfig["epoch"],
+            eta_min=0,last_epoch= -1)
+    warmUpScheduler = GradualWarmupScheduler(
+            optimizer = optimizer, 
+            multiplier=modelConfig["multiplier"], 
+            warm_epoch= modelConfig["epoch"] // 10, 
+            after_scheduler=cosineScheduler)
+    
+    # Diffusion trainer
+    trainer = GaussianDiffusionTrainer(
+            net_model, 
+            modelConfig["beta_1"],
+            modelConfig["beta_T"],
+            modelConfig["T"]
+            ).to(device)
+    
+    os.makedirs(modelConfig["save_weight_dir"], exist_ok=True)
 
-        #start training
+    # start training
+    for e in range(modelConfig["epoch"]):
+        net_model.train()
 
-        for e in range(modelConfig["epoch"]):
-            with tqdm(dataloader, dynmaic_ncols=True) as tqdmDataLoader:
-                for images, labels in tqdmDataLoader:
+        with tqdm(dataloader, 
+                  dynamic_ncols=True
+                  ) as tqdmDataLoader:
+                for images, _ in tqdmDataLoader:
                     # train
                     optimizer.zero_grad()
-                    x_0 = images.to(device)
+
+                    x_0 = images.to(device, non_blocking = True)
+
                     loss = trainer(x_0).sum() / 1000.
+                    # loss = trainer(x_0).mean()
+                    
                     loss.backward()
+                    
                     torch.nn.utils.clip_grad_norm_(
-                        net_model.parameters(), modelConfig["grad_clip"])
+                        net_model.parameters(), 
+                        modelConfig["grad_clip"]
+                    )
+                    
                     optimizer.step()
+                    
                     tqdmDataLoader.set_postfix(ordered_dict={
-                        "epoch" : e,
+                        "epoch" : e #+ 1,
                         "loss: ": loss.item(),
                         "img shape: ": x_0.shape,
-                        "LR": optimizer.state_dict()['para_groups'][0]["lr"]
-                    })
-
+                        #"LR": optimizer.state_dict()['para_groups'][0]["lr"]
+                        "LR": optimizer.state_dict()['param_groups'][0]["lr"]
+                         }
+                    )
+        # Update learning rate once after each epoch
         warmUpScheduler.step()
-        torch.save(net_model.state_dict(), os.path.join(
-            modelConfig["save_weight_dir"], 'ckpt_' + str(e) + "_.pt"))
-                 
+
+        # save checkpoint
+        checkpoint_path = os.path.join(
+             modelConfig["save_weight_dir"],
+             f"ckpt_{e+1}.pt"
+        )
+
+        torch.save(net_model.state_dict(), checkpoint_path)
+                             
